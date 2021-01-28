3401ab5f9822a568aabc1d961a8a97492729f0c3
DaniyarKulmanov <DanRubyDeveloper@gmail.com>
2021-01-28 12:19:07 +0600

    triangle complete

--- Lesson_2/triangle.rb
+++ Lesson_2/triangle.rb
@@ -6,3 +6,9 @@ bottom = gets.chomp.to_i
 puts "Какой длины высота треугольника?"
 height = gets.chomp.to_i
 
+def count_area ( bottom, height )
+  0.5 * bottom * height
+end
+
+area = count_area(bottom, height)
+puts "Площадь треугольника = #{area}"
