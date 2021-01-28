fb45f8c0cbec793074ad5bd93a44c1c714126cb9
DaniyarKulmanov <DanRubyDeveloper@gmail.com>
2021-01-28 11:54:30 +0600

    Interface

---
+++ Lesson_2/perfect_weight.rb
@@ -0,0 +1,11 @@
+puts "Вас привествует программа идеального Веса!"
+
+puts "Здравствуйте как Вас зовут?"
+name = gets.chomp
+
+puts "Введите Ваш рост"
+height = gets.chomp
+
+puts "Введите Ваш вес"
+weight = gets.chomp
+
