0ae9ce592837781a2eacc2cf56badb156c646738
DaniyarKulmanov <DanRubyDeveloper@gmail.com>
2021-01-29 14:15:22 +0600

    quadric final

--- Lesson_2/quadratic_equation.rb
+++ Lesson_2/quadratic_equation.rb
@@ -2,7 +2,7 @@ puts "Вас привествует программа квадратное ур
 puts "Введите 3 коэффицента"
 
 def coefficient (name)
-  puts "Длина стороны #{name} ="
+  puts "Коэффицент #{name} ="
   gets.chomp.to_i
 end
 
@@ -10,3 +10,15 @@ a = coefficient "a"
 b = coefficient "b"
 c = coefficient "c"
 
+d = b**2 - (4 * a * c)
+
+if d < 0
+  puts "корней нет"
+elsif d == 0
+  x1 = ( -b + Math.sqrt(d) ) / 2 * a
+  puts "Дискриминант = #{d}, корень = #{x1}"
+else
+  x1 = ( -b + Math.sqrt(d) ) / 2 * a
+  x2 = ( -b - Math.sqrt(d) ) / 2 * a
+  puts "Дискриминант = #{d}, корень 1 = #{x1}, корень 2 = #{x2}"
+end
