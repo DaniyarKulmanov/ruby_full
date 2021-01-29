8c6845f3003c4e49acc7a56a2b8b23f40a29fcf8
DaniyarKulmanov <DanRubyDeveloper@gmail.com>
2021-01-29 13:29:24 +0600

    finish right_triangle

--- Lesson_2/right_triangle.rb
+++ Lesson_2/right_triangle.rb
@@ -3,35 +3,19 @@ def enter_side (name)
   gets.chomp.to_i
 end
 
-def equilateral (a,b,c)
-  a == b && b == c
-end
-
-
-def find_hypotenuse (a,b,c)
-  if a > b && a > c
-    a
-  elsif b > a && b > c
-    b
-  elsif c > a && c > b
-    c
-  else
-    false
-  end
-end
-
 puts "Введите три стороны треугольника по очереди."
 
-side_a = enter_side "A"
-side_b = enter_side "B"
-side_c = enter_side "C"
+a = enter_side "A"
+b = enter_side "B"
+c = enter_side "C"
 
-# check equilateral?
-if equilateral side_a, side_b, side_c
+if a > b && a > c
+  puts "Треугольник прямоугольный" if a**2 == b**2 + c**2
+elsif b > a && b > c
+  puts "Треугольник прямоугольный" if b**2 == a**2 + c**2
+elsif c > a && c > b
+  puts "Треугольник прямоугольный" if c**2 == b**2 + a**2
+elsif a == b && b == c
   puts "Треугольник равносторонний"
 end
 
-hypotenuse = find_hypotenuse side_a, side_b, side_c
-if hypotenuse != false
-
-end
