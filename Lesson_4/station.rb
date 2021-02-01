62ff61d2af4cc832d4cb1d77507a9e8b18867c2a
DaniyarKulmanov <DanRubyDeveloper@gmail.com>
2021-02-01 18:35:16 +0600

    Finish train and station

--- Lesson_4/station.rb
+++ Lesson_4/station.rb
@@ -7,13 +7,24 @@ class Station
   end
 
   def arrive (train)
-    trains << train
+    self.trains << train
   end
 
   def trains_by_type
+    puts "Количество поздов по типу:"
+    puts "Грузовые #{train_info('Cargo')}"
+    puts "Пассажирские #{train_info('Passenger')}"
   end
 
   def departure (train)
-    trains.delete(train)
+    self.trains.delete(train)
+  end
+
+  def train_info(type)
+    count = 0
+    self.trains.each do |train|
+      count += 1 if train.type == type
+    end
+    count
   end
 end
--- Lesson_4/train.rb
+++ Lesson_4/train.rb
@@ -1,7 +1,7 @@
 class Train
 
   attr_reader :number, :type
-  attr_accessor :speed, :wagons, :route, :current_station
+  attr_accessor :speed, :wagons, :route, :station
 
   def initialize(number, type, wagons)
     @number = number
@@ -23,16 +23,38 @@ class Train
   end
 
   def unhitch_wagon
-    self.wagons =+ 1 if speed == 0 && self.wagons > 0
+    self.wagons -= 1 if speed == 0 && self.wagons > 0
   end
 
   def add_route (route)
     self.route = route
-    self.current_station = route.stations.first
-    self.current_station.arrive(self)
+    self.route.stations.first.arrive(self)
+    self.station = 0
   end
 
   def move_forward
-    # self.route.stations.find{|station| }
+    move(1)
+  end
+
+  def move_back
+    move(-1)
+  end
+
+  def information
+    station_read((self.station - 1), "предыдущая станция") if self.station > 0
+    station_read(self.station, "текущая станция")
+    station_read((self.station + 1), "следующая станция")
+  end
+
+  private
+
+  def move(direction)
+    self.route.stations[station].departure(self)
+    self.route.stations[station + direction].arrive(self)
+    self.station += direction
+  end
+
+  def station_read(index, text)
+    puts "#{text} = #{self.route.stations[index].name}"
   end
 end
