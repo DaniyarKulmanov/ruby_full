291b3185ee6aec36146a1a40c97eb80174d0f8a4
DaniyarKulmanov <DanRubyDeveloper@gmail.com>
2021-02-03 18:18:39 +0600

    wagon.rb done

--- Lesson_5/wagon.rb
+++ Lesson_5/wagon.rb
@@ -1,3 +1,21 @@
 class Wagon
+  attr_reader :manufacturer, :open_locks
+
+  def initialize(manufacturer)
+    @manufacturer = manufacturer
+    close_doors
+  end
+
+  def open_doors
+    self.open_locks = true
+  end
+
+  def close_doors
+    self.open_locks = false
+  end
+
+  private
+
+  attr_writer :open_locks
 
 end
