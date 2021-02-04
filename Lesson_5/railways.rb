12dcbe0dc262f0b81f309bcd1e329e7f90dde6cc
DaniyarKulmanov <DanRubyDeveloper@gmail.com>
2021-02-04 17:42:59 +0600

    menu_texts.rb

--- Lesson_5/railways.rb
+++ Lesson_5/railways.rb
@@ -4,10 +4,10 @@ require_relative 'passenger_wagon'
 require_relative 'cargo_wagon'
 require_relative 'route'
 require_relative 'station'
+require_relative 'menu_texts'
 
 class RailWays
   #TODO создать констатны под тексты
-  MAIN_MENU = ['Основное меню',  '1 - Станции', '2 - Маршруты', '3 - Поезда']
   attr_reader :routes, :stations, :trains, :wagons
 
   def initialize
@@ -20,25 +20,10 @@ class RailWays
 
   def main_menu
     command = paint_menu MAIN_MENU
-    
     case command
-    when 1
-      station_actions paint_menu "Меню станций",
-                                 "1 - Создать",
-                                 "2 - Просмотр"
-    when 2
-      route_actions paint_menu "Меню маршрутов",
-                               "1 - Создать",
-                               "2 - Просмотр",
-                               "3 - Добавить стнацию",
-                               "4 - Удалить станцию"
-
-    when 3
-      train_menu paint_menu "Меню поездов",
-                             "1 - Создать",
-                             "2 - Просмотр",
-                             "3 - Назначить маршрут поезду",
-                             "4 - Добавить вагон поезду"
+    when 1 then station_actions(paint_menu STATION_MENU)
+    when 2 then route_actions(paint_menu ROUTE_MENU)
+    when 3 then train_actions(paint_menu TRAIN_MENU)
     end
   end
 
@@ -46,11 +31,11 @@ class RailWays
 
   attr_writer :routes, :stations, :trains, :wagons
 
-  def paint_menu (header, item1 = nil , item2 = nil, item3 = nil, item4 = nil)
+  def paint_menu (header)
     command = 0
     loop do
       # system('clear')
-      puts header, "0 - Выход", item1, item2, item3, item4
+      puts header
       input = gets.chomp
       if input == '0'
         command = input.to_i
@@ -74,12 +59,12 @@ class RailWays
     puts "Введите имя станции"
     name = gets.chomp
     self.stations << Station.new(name)
-    station_actions paint_menu "Меню станций", "1 - Создать", "2 - Просмотр"
+    station_actions(paint_menu STATION_MENU)
   end
 
   def stations_display
     stations_list
-    station_actions paint_menu"Меню станций","1 - Создать","2 - Просмотр"
+    station_actions(paint_menu STATION_MENU)
   end
 
   def stations_list
@@ -111,18 +96,12 @@ class RailWays
     last_station = stations[index]
 
     routes << Route.new(first_station, last_station)
-    route_actions paint_menu "Меню маршрутов",
-                             "1 - Создать",
-                             "2 - Просмотр"
+    route_actions(paint_menu ROUTE_MENU)
   end
 
   def routes_display
     routes_list
-    route_actions paint_menu"Меню маршрутов",
-                             "1 - Создать",
-                             "2 - Просмотр",
-                             "3 - Добавить стнацию",
-                             "4 - Удалить станцию"
+    route_actions(paint_menu ROUTE_MENU)
   end
 
   def routes_list
@@ -130,6 +109,7 @@ class RailWays
     routes.each_with_index do |route, index|
       puts "#{index} - Станции маршрута:"
       route.stations.each {|station| puts "-> #{station.name}"}
+      #TODO список поездов на стнации
     end
   end
 
@@ -143,11 +123,7 @@ class RailWays
     station_index = gets.chomp.to_i
 
     routes[route_index].add_station(stations[station_index])
-    route_actions paint_menu"Меню маршрутов",
-                            "1 - Создать",
-                            "2 - Просмотр",
-                            "3 - Добавить стнацию",
-                            "4 - Удалить станцию"
+    route_actions(paint_menu ROUTE_MENU)
   end
 
   def routes_del_station
@@ -161,15 +137,11 @@ class RailWays
     station_to_delete = routes[route_index].stations[station_index]
 
     routes[route_index].remove_station(station_to_delete)
-    route_actions paint_menu"Меню маршрутов",
-                            "1 - Создать",
-                            "2 - Просмотр",
-                            "3 - Добавить стнацию",
-                            "4 - Удалить станцию"
+    route_actions(paint_menu ROUTE_MENU)
   end
 
   # routes ========
-  def train_menu(command)
+  def train_actions(command)
   end
 
   def seed
