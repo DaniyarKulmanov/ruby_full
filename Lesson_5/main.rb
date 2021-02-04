334c154b1ff33323920b7053b1340dabbc3ff513
DaniyarKulmanov <DanRubyDeveloper@gmail.com>
2021-02-04 12:31:34 +0600

    railways logic

--- Lesson_5/main.rb
+++ Lesson_5/main.rb
@@ -1,10 +1,4 @@
-require_relative 'passenger_train'
-require_relative 'cargo_train'
-require_relative 'passenger_wagon'
-require_relative 'cargo_wagon'
-require_relative 'route'
-require_relative 'station'
 require_relative 'railways'
 
 kazakhstan_railways = RailWays.new
-kazakhstan_railways.launch_program
+kazakhstan_railways.main_menu
--- Lesson_5/railways.rb
+++ Lesson_5/railways.rb
@@ -1,20 +1,42 @@
+require_relative 'passenger_train'
+require_relative 'cargo_train'
+require_relative 'passenger_wagon'
+require_relative 'cargo_wagon'
+require_relative 'route'
+require_relative 'station'
+
 class RailWays
+  #TODO создать констатны под тексты
   attr_reader :routes, :stations, :trains, :wagons
 
-  def launch_program
-    loop do
-      puts "Добро пожаловать в Железную Дорогу!",
-           "Введите номер меню для дальнейших действий:",
-           "1 - Станции",
-           "2 - Маршруты",
-           "3 - Поезда",
-           "0 - Выход"
-      input = gets.chomp
-      if input == '0'
-        break
-      elsif input.to_i.between?(1,3)
-        sub_menu(input.to_i)
-      end
+  def initialize
+    @stations = []
+    @routes = []
+    @trains = []
+    @wagons = []
+  end
+
+  def main_menu
+    command = paint_menu "Основное меню",
+                         "1 - Станции",
+                         "2 - Маршруты",
+                         "3 - Поезда"
+    case command
+    when 1
+      station_menu paint_menu"Меню станций",
+                          "1 - Создать",
+                          "2 - Просмотр"
+    when 2
+      route_menu paint_menu "Меню маршрутов",
+                         "1 - Создать",
+                         "2 - Просмотр"
+                 # "3 - Добавить станцию",
+                 # "4 - Удалить станцию"
+
+    when 3
+      train_menu paint_menu "Меню поездов",
+                         "1 - Создать",
+                         "2 - Просмотр"
     end
   end
 
@@ -22,21 +44,52 @@ class RailWays
 
   attr_writer :routes, :stations, :trains, :wagons
 
-  def sub_menu(input)
-    stations if input == 1
-    routes if input == 2
-    trains if input == 3
+  def paint_menu (header, item1 = nil , item2 = nil, item3 = nil, item4 = nil)
+    command = 0
+    loop do
+      # system('clear')
+      puts header, "0 - Выход", item1, item2, item3, item4
+      input = gets.chomp
+      if input == '0'
+        command = input.to_i
+        break
+      elsif input.to_i.between?(1,3)
+        command = input.to_i
+        break
+      end
+    end
+    command
   end
 
-  def stations
-    puts 'stations'
+  def station_menu(command)
+    puts "station_menu #{command}"
+    station_create if command == 1
+    stations_display if  command == 2
+    main_menu if command == 0
   end
 
-  def routes
-    puts 'routes'
+  def station_create
+    puts "Введите имя станции"
+    name = gets.chomp
+    self.stations << Station.new(name)
+    station_menu paint_menu"Меню станций",
+                           "1 - Создать",
+                           "2 - Просмотр"
   end
 
-  def trains
-    puts 'trains'
+  def stations_display
+    puts "Список станций:"
+    stations.each_with_index do |station, index|
+      puts "#{index + 1} - #{station.name}"
+    end
+    station_menu paint_menu"Меню станций",
+                           "1 - Создать",
+                           "2 - Просмотр"
+  end
+
+  def route_menu(command)
+  end
+
+  def train_menu(command)
   end
 end
